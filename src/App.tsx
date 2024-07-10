140dc9a7b4e6ebf71814a8d8832ec37c0ad81284
Gabe Serna <gabriel.serna118@gmail.com>
2024-07-09 20:40:35 -0400

    reset handleNewWord function

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import React from 'react';
 function App() {
   const [mysteryWordArray, setMysteryWordArray] = useState([] as string[]);
   const handleNewWord = () => {
-    generateWord(setMysteryWordArray, '', true);
+    generateWord(setMysteryWordArray);
   };
 
   useEffect(() => {
